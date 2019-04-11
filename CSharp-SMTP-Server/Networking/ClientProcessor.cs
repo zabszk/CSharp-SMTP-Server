2d739d3cec5b1e51c92104e42f7a92095dd15333
Łukasz Jurczyk <zabszk@protonmail.ch>
2019-04-11 21:32:24 +0200

    Development

---
+++ CSharp-SMTP-Server/Networking/ClientProcessor.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace CSharp_SMTP_Server.Networking
+{
+	internal class ClientProcessor : IDisposable
+	{
+		internal ClientProcessor(TcpClient c, Listener l)
+		{
+			_listener = l;
+			_buffer = new byte[BufferSize];
+			_client = c;
+			_stream = c.GetStream();
+			_encoder = new UTF8Encoding();
+
+			_clientThread = new Thread(Receive)
+			{
+				Name = "Receive thread",
+				IsBackground = true
+			};
+		}
+
+		public const int BufferSize = 1024;
+
+		private readonly TcpClient _client;
+		private readonly NetworkStream _stream;
+		private readonly Thread _clientThread;
+		private readonly Listener _listener;
+		private byte[] _buffer;
+		private UTF8Encoding _encoder;
+
+		private void Receive()
+		{
+			while (true)
+			{
+				if (_client.Available == 0)
+				{
+					Thread.Sleep(2);
+					continue;
+				}
+
+				using (var memoryStream = new MemoryStream())
+				{
+					var bytesRead = 0;
+					while ((bytesRead = _stream.Read(_buffer, 0, BufferSize)) > 0)
+						memoryStream.Write(_buffer, 0, bytesRead);
+
+					ProcessResponse(_encoder.GetString(memoryStream.ToArray()));
+				}
+			}
+		}
+
+		private void ProcessResponse(string response)
+		{
+
+		}
+
+		public void Dispose()
+		{
+			_clientThread.Abort();
+			_client?.Dispose();
+			_stream?.Dispose();
+
+			if (_listener != null && _listener.ClientProcessors.Contains(this))
+				_listener.ClientProcessors.Remove(this);
+		}
+	}
+}
---
+++ CSharp-SMTP-Server/Networking/Listener.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharp_SMTP_Server.Networking
+{
+	public class Listener : IDisposable
+	{
+		private readonly TcpListener _listener;
+		private readonly Thread _listenerThread;
+		private readonly IPEndPoint _ipEndPoint;
+		internal readonly List<ClientProcessor> ClientProcessors;
+
+		public Listener(IPAddress address, ushort port)
+		{
+			ClientProcessors = new List<ClientProcessor>();
+
+			_ipEndPoint = new IPEndPoint(address, port);
+			_listener = new TcpListener(_ipEndPoint);
+			_listenerThread = new Thread(Listen)
+			{
+				Name = "Listening on port " + port,
+				IsBackground = true
+			};
+		}
+
+		internal void Start() => _listenerThread.Start();
+
+		private void Listen()
+		{
+			_listener.Start(200);
+			while (true)
+			{
+				var client = _listener.AcceptTcpClient();
+				ClientProcessors.Add(new ClientProcessor(client, this));
+			}
+		}
+
+		public void Dispose()
+		{
+			_listenerThread.Abort();
+			_listener.Stop();
+		}
+	}
+}
