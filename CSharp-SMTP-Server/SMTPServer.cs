98ca1ff67d3c4961402413d8e523b28ce192b965
Łukasz Jurczyk <zabszk@protonmail.ch>
2023-10-12 16:42:20 +0200

    Bumped version to 1.1.5

--- CSharp-SMTP-Server/SMTPServer.cs
+++ CSharp-SMTP-Server/SMTPServer.cs
@@ -22,7 +22,7 @@ namespace CSharp_SMTP_Server
 		/// <summary>
 		/// Library version
 		/// </summary>
-		public const string VersionString = "1.1.4";
+		public const string VersionString = "1.1.5";
 
 		/// <summary>
 		/// Server options
